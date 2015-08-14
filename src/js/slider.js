79797637fd253bedeeb75ce8c31a3e762c8fa83f
Andrey Balandin <vinc@fromru.com>
2015-08-14 14:51:45 +0500

    Слайдер с фотографиями продукта
    - сверстана вставка со слайдером отдельным jade (slider.jade, slider.scss)
    - добавлены фото для слайдера (img/product/)
    - написан Javascript для слайдера (slider.jade)
    - функционал только для трех картинок в списке превью, без стрелок влево-вправо
    - установлен jQuery 1.11.3

---
+++ src/js/slider.js
@@ -0,0 +1,42 @@
+// Модуль работы со слайдером
+'use strict';
+
+var slider = (function (){
+
+  var init = function() {
+      console.log('Инициализация модуля slider.js');
+      _setUpListners();
+  };
+
+   // установка обработчиков событий
+  var _setUpListners = function () {
+      $('.product-slider__pic').on('click', _changePic);
+  };
+
+  // сменить картинку
+  var _changePic = function (event) {
+    event.preventDefault();
+
+    var pic = $(this),
+        item = pic.closest('.product-slider__item'),
+        container = pic.closest('.product-slider'),
+        display = container.find('.product-slider__display'),
+        path = item.find('img').attr('src'),
+        duration = 150;
+
+    if (!item.hasClass('active')) {
+        item.addClass('active').siblings().removeClass('active');
+        display.find('img').fadeOut(duration, function() {
+            $(this).attr('src', path).fadeIn(duration);
+        });
+    };
+
+  };
+
+  return {
+    init: init
+  };
+
+})();
+
+slider.init();
